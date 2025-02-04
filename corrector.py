2968c1ebe1593c5b3f01481071d9bf6736e1ce6c
Zeegar <61349777+Zeegar@users.noreply.github.com>
2025-02-04 13:03:34 +0000

    Update corrector.py
    added multiple keyword handling

--- corrector.py
+++ corrector.py
@@ -11,12 +11,23 @@ class Corrector:
             if not line_stripped:
                 corrected_lines.append(line)
                 continue
-            keyword = line_stripped.split()[0]
-            if keyword not in self.valid_keywords:
-                closest_matches = difflib.get_close_matches(keyword, self.valid_keywords, n=1, cutoff=0.8)
+
+            # Check for multi-word keywords
+            keyword = None
+            for valid_keyword in self.valid_keywords:
+                if line_stripped.startswith(valid_keyword):
+                    keyword = valid_keyword
+                    break
+
+            if keyword is None:
+                # If no valid keyword is found, attempt to correct the first word
+                first_word = line_stripped.split()[0]
+                closest_matches = difflib.get_close_matches(first_word, self.valid_keywords, n=1, cutoff=0.8)
                 if closest_matches:
-                    corrected_line = closest_matches[0] + line_stripped[len(keyword):]
+                    corrected_line = closest_matches[0] + line_stripped[len(first_word):]
                     corrected_lines.append(corrected_line)
+                else:
+                    corrected_lines.append(line)
             else:
                 corrected_lines.append(line)
         return corrected_lines
