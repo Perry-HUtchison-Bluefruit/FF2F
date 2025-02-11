52843d03076c8be906f767457d238fd6174d50cf
Zeegar <61349777+Zeegar@users.noreply.github.com>
2025-02-11 15:21:36 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     def __init__(self):
-        self.keywords = ['Feature:', 'Scenario:', 'Scenario Outline:', 'Given', 'When', 'Then', 'And', 'Examples:', '|']
+        self.keywords = ['Feature:', 'Scenario:', 'Scenario Outline:', 'Developer Task:', 'Given', 'When', 'Then', 'And', 'Examples:', '|']
 
     def get_keywords(self):
         return self.keywords
